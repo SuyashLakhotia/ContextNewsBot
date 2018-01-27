dd83d4a7583bc6fa6ace1dc9cae1befa94311b03
Chaitanya Joshi <ckjoshi9@gmail.com>
2018-01-27 15:55:09 +0800

    Updated functions and exec test

--- determine_relevance.py
+++ determine_relevance.py
@@ -1,20 +1,33 @@
 import paralleldots
 
 
-class RelevanceDeterminer(object):
-	def __init__(self, threshold):
+class RelevanceDeterminer:
+	def __init__(self, threshold=3.5):
 		self.threshold = threshold
 
 		paralleldots.set_api_key("siQChQ9PVPRs8Gm0HDawsqscverucbEq77zNBZpNXI8")
 
-	def get_relevant_news(tweet, news_set, threshold=self.threshold):
+	def get_relevant_news(self, tweet, news_set):
 		relevant_news_set = []
 		for item in news_set:
-			if _relevance_score(tweet, item) >= threshold:
-				relevant_news_set.append(item):
+			if self._relevance_score(tweet, item) >= self.threshold:
+				relevant_news_set.append(item)
+		return relevant_news_set
 
-	def _relevance_score(tweet, news_item):
-		# TODO depends on structure of news_item
-		paralleldots.similarity(tweet, news_item)
+	def _relevance_score(self, tweet, news_item):
+		# TODO depends on structure of news_item and API response
+		api_response = paralleldots.similarity(tweet, news_item)
+		return api_response['normalized_score']
 
 
+if __name__ == '__main__':
+	RelevanceDeterminer = RelevanceDeterminer(3.5)
+	
+	tweet = "Donald Trump is correct about global warming. Total farce."
+	news_set = [
+				"Prime Minister Modi gives very good speeches.", 
+				"Global warming is scientifically true. Researchers have found evidence.", 
+				"Donald Trump lies about facts nine out of ten times."
+				]
+
+	print(RelevanceDeterminer.get_relevant_news(tweet, news_set))
