918c8e530171b19ad330498d3ad62650708207ba
Chaitanya Joshi <ckjoshi9@gmail.com>
2018-01-27 19:03:23 +0800

    Added options for relevence score

--- determine_relevance.py
+++ determine_relevance.py
@@ -1,12 +1,12 @@
 import paralleldots
+import requests
 
 from news_articles_retriever import NewsArticlesRetriever, pretty_print_news
+
 
 class RelevanceDeterminer:
     def __init__(self, threshold=3.5):
         self.threshold = threshold
-
-        paralleldots.set_api_key("siQChQ9PVPRs8Gm0HDawsqscverucbEq77zNBZpNXI8")
 
     def get_relevant_news(self, tweet, news_articles):
         """
@@ -25,10 +25,27 @@ class RelevanceDeterminer:
                 relevant_news_articles.append(item)
         return relevant_news_articles
 
-    def _relevance_score(self, tweet, news_item):
+    def _relevance_score(self, tweet, news_item, key="paralleldots"):
         # TODO depends on structure of news_item and API response
-        api_response = paralleldots.similarity(tweet, news_item)
-        return api_response['normalized_score']
+        
+        if key == "paralleldots":
+            paralleldots.set_api_key("siQChQ9PVPRs8Gm0HDawsqscverucbEq77zNBZpNXI8")
+            api_response = paralleldots.similarity(tweet, news_item)
+            return api_response['normalized_score']
+
+        elif key == "dandelion":
+            base_url = "https://api.dandelion.eu/datatxt/sim/v1/"
+            text1 = tweet.replace(" ", "%20")
+            text2 = news_item.replace(" ", "%20")
+            api_key = "9e90047b495448adbab611edd14fae3e"
+
+            url_semantic = base_url + '?text1=' + text1 + '&text2=' + text2 + '&bow=never' + "&token=" + api_key
+            api_response_semantic = requests.post(url_semantic).json()
+
+            url_syntactic = base_url + '?text1=' + text1 + '&text2=' + text2 + '&bow=always' + "&token=" + api_key
+            api_response_syntactic = requests.post(url_semantic).json()
+
+            return [api_response_semantic['similarity'], api_response_syntactic['similarity']]
 
 
 if __name__ == '__main__':
